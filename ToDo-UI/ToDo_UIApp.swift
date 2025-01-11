07c20ee5e029e15decca9833a4da7814b42978cc
Furkan Ayaz <furkan.yz@outlook.com>
2025-01-11 20:45:39 +0300

    Initial Commit

---
+++ ToDo-UI/ToDo_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_UIApp.swift
+//  ToDo-UI
+//
+//  Created by FURKAN AYAZ on 11.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
